416a45da8ea051d4246652ee1a52600b9c21abb2
snailfighting <vip.milael@gmail.com>
2020-10-13 20:31:41 +0300

    Scripted auto-commit on change (2020-10-13 20:31:41) by gitwatch.sh

--- src/main/java/laba2/FlightJob.java
+++ src/main/java/laba2/FlightJob.java
@@ -10,11 +10,16 @@ public class FlightJob {
         public static void main(String[] args) throws Exception {
             if (args.length != 2) {
                 System.err.println("Usage: WordCountApp <input path> <output path>");
-                System.exit(-1); ...
+                System.exit(-1);
             }
             Job job = Job.getInstance();
             job.setJarByClass(FlightJob.class);
             job.setJobName("Word count");
             //....//
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            job.setMapperClass(WordMapper.class);
+            job.setReducerClass(WordReducer.class);
         }
 }
