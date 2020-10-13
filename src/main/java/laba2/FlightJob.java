54a217de30de73db2e777bc57c727c4db4188375
snailfighting <vip.milael@gmail.com>
2020-10-13 20:29:49 +0300

    Scripted auto-commit on change (2020-10-13 20:29:49) by gitwatch.sh

---
+++ src/main/java/laba2/FlightJob.java
@@ -0,0 +1,20 @@
+package laba2;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FlightJob {
+        public static void main(String[] args) throws Exception {
+            if (args.length != 2) {
+                System.err.println("Usage: WordCountApp <input path> <output path>");
+                System.exit(-1); ...
+            }
+            Job job = Job.getInstance();
+            job.setJarByClass(FlightJob.class);
+            job.setJobName("Word count");
+            //....//
+        }
+}
