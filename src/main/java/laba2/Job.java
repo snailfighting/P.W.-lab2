d48152e6996cd65189baf1c70d3bbb5ba0015a96
snailfighting <vip.milael@gmail.com>
2020-10-13 20:18:59 +0300

    Scripted auto-commit on change (2020-10-13 20:18:59) by gitwatch.sh

--- src/main/java/laba2/Job.java
+++ src/main/java/laba2/Job.java
@@ -1,5 +1,11 @@
 package laba2;
 
 public class Job {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
 
+    }
 }
