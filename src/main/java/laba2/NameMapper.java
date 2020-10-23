0bedeacb89fba4c798147da93e812eb3d8e1f325
snailfighting <vip.milael@gmail.com>
2020-10-23 14:11:41 +0300

    Scripted auto-commit on change (2020-10-23 14:11:41) by gitwatch.sh

--- src/main/java/laba2/NameMapper.java
+++ src/main/java/laba2/NameMapper.java
@@ -8,4 +8,5 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class NameMapper extends Mapper<LongWritable, Text,Text,IntWritable> {
+    private static final String DELIMITER = "\,";
 }
