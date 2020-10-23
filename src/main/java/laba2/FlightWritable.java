8a905cba4b55b4a5525643deb32671336950222a
snailfighting <vip.milael@gmail.com>
2020-10-23 14:38:23 +0300

    Scripted auto-commit on change (2020-10-23 14:38:23) by gitwatch.sh

--- src/main/java/laba2/FlightWritable.java
+++ src/main/java/laba2/FlightWritable.java
@@ -17,8 +17,13 @@ public class FlightWritable implements Writable {
     public FlightWritable(){}
 
     public FlightWritable(float arr_delay, float cancelled, float air_time, int dest_aeroport_id){
-        
+        this.air_time = air_time;
+        this.arr_delay = arr_delay;
+        this.cancelled = cancelled;
+        this.dest_aeroport_id = dest_aeroport_id;
     }
+
+    
 
 
 
