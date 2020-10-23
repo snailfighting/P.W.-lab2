8566f15a46ea837e47f1c9ed6103e0c885b23c81
snailfighting <vip.milael@gmail.com>
2020-10-23 14:14:27 +0300

    Scripted auto-commit on change (2020-10-23 14:14:27) by gitwatch.sh

--- src/main/java/laba2/NameMapper.java
+++ src/main/java/laba2/NameMapper.java
@@ -10,5 +10,10 @@ import java.io.IOException;
 public class NameMapper extends Mapper<LongWritable, Text,Text,IntWritable> {
     private static final String DELIMITER = "\",";
     private static final int DEST_AEROPORT_ID = 0;
-    private static final int NAME_AEROPORT = 0;
+    private static final int NAME_AEROPORT = 1;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
