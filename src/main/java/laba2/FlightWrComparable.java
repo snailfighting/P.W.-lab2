963ce04c8c91a1766ea691fabd85b22dd67f10b6
snailfighting <vip.milael@gmail.com>
2020-10-23 14:48:38 +0300

    Scripted auto-commit on change (2020-10-23 14:48:38) by gitwatch.sh

--- src/main/java/laba2/FlightWrComparable.java
+++ src/main/java/laba2/FlightWrComparable.java
@@ -2,6 +2,7 @@ package laba2;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
@@ -14,6 +15,11 @@ public class FlightWrComparable implements WritableComparable {
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
 
     }
 }
