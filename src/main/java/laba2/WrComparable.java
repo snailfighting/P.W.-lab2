405d56cdb401fc9e9d7c0ef185b21b3d0c9f4508
snailfighting <vip.milael@gmail.com>
2020-10-23 14:24:07 +0300

    Scripted auto-commit on change (2020-10-23 14:24:07) by gitwatch.sh

--- src/main/java/laba2/WrComparable.java
+++ src/main/java/laba2/WrComparable.java
@@ -3,5 +3,6 @@ package laba2;
 import org.apache.hadoop.io.WritableComparable;
 
 public class WrComparable implements WritableComparable{
-    private  int aeroportID 
+    private  int aeroportID;
+    private int 
 }
