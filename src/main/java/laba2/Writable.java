3e96b5178e602b4432ec33c707dddee749cda2b6
snailfighting <vip.milael@gmail.com>
2020-10-23 14:26:52 +0300

    Scripted auto-commit on change (2020-10-23 14:26:52) by gitwatch.sh

--- src/main/java/laba2/Writable.java
+++ src/main/java/laba2/Writable.java
@@ -1,5 +1,12 @@
 package laba2;
 
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+
 public class Writable implements Writable {
     
 }
