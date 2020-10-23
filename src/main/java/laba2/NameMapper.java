ce701932c7563d1ec8972a8e5240b50358731154
snailfighting <vip.milael@gmail.com>
2020-10-23 14:16:02 +0300

    Scripted auto-commit on change (2020-10-23 14:16:02) by gitwatch.sh

--- src/main/java/laba2/NameMapper.java
+++ src/main/java/laba2/NameMapper.java
@@ -16,6 +16,7 @@ public class NameMapper extends Mapper<LongWritable, Text,Text,IntWritable> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if(key.get() > 0){
             String[] table = value.toString().split(DELIMITER);
+            int dest_aeroport_id = Integer.parseInt(table[DEST_AEROPORT_ID]);
         }
     }
 }
