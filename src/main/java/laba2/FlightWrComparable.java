05873a326e77728c2a5c9d3b5fcd347862b0a683
snailfighting <vip.milael@gmail.com>
2020-10-23 14:48:08 +0300

    Scripted auto-commit on change (2020-10-23 14:48:08) by gitwatch.sh

--- src/main/java/laba2/FlightWrComparable.java
+++ src/main/java/laba2/FlightWrComparable.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.WritableComparable;
 public class FlightWrComparable implements WritableComparable {
 
     public int aeroport_id;
-    public int 
+    public int indenity;
+
+    FlightWrComparable(){}
 
 }
