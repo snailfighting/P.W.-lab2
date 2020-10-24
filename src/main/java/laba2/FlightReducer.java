23d11521eaee9a8b18faaac59995942fd4f1d527
snailfighting <vip.milael@gmail.com>
2020-10-24 17:22:19 +0300

    Scripted auto-commit on change (2020-10-24 17:22:19) by gitwatch.sh

--- src/main/java/laba2/FlightReducer.java
+++ src/main/java/laba2/FlightReducer.java
@@ -12,8 +12,10 @@ public class FlightReducer extends Reducer<FlightWrComparable,Text,Text,LongWrit
 
     @Override
     protected void reduce(FlightWrComparable key, Iterable<Text> values, Context context) throws IOException,InterruptedException{
+        float max = 
+
         Iterator<Text> iterator = values.iterator();
         Text inf = new Text(iterator.next());
-        
+
     }
 }
